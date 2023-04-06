ce13e4d40f5ba2cdfa5fdd2a3d8cbd331203ac82
lokesh817 <lokeshkumarparihar143@gmail.com>
2023-04-06 09:59:28 +0530

    setup our ejs view engine and path

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const  route  = require('./routes');
 const app=express();
 const port=8000;
 
+//setup default route 
+app.use('/',require('./routes/index'));
 
-app.use('/',require('./routes/index'))
-
+//set up view engine and set path views
+app.use('view engine','ejs');
+app.set('views','./views');
 
 
 app.listen(port,function(err){
