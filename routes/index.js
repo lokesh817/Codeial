21d8135c876470c2ac06d3e1a08103c38972d575
lokesh817 <lokeshkumarparihar143@gmail.com>
2023-04-06 09:51:01 +0530

    added users and post routes and controller file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 const express=require('express');
 const router=express.Router();
 const home_controller=require('../controllers/home_controller');
+
 console.log('router loaded');
+
+
+
 router.get('/',home_controller.home);
 router.get('/friends',home_controller.friends);
-router.get('/post',home_controller.post);
 router.get('/reel',home_controller.reel);
+
+
+
+//since this file our main route we are diverting it to user whenever we need transfer request for user
+router.use('/users',require('./users'));
+router.use('/post',require('./post'));
 module.exports=router;
---
+++ routes/post.js
@@ -0,0 +1,6 @@
+const express=require('express');
+const router=express.Router();
+const postController=require('../controllers/post_controller');
+router.get('/',postController.post);
+
+module.exports=router;
